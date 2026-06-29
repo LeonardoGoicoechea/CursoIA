a74ed1e6f883d429fcdfde907224ef7a607f6070
LeonardoGoicoechea <LeonardoGoicoechea@users.noreply.github.com>
2026-06-28 23:03:59 -0300

    Remove privacy warning banner

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ window.COURSE_REGISTRATION_CONFIG = {
   googleAppsScriptUrl: "https://script.google.com/macros/s/AKfycbzWwijIWQDt1UH6LHnvLBwHyG7uiTkXOvwxnnZWvHA6XMhFkDYLbZj32LZIJRIH1-FG/exec",
   apiToken: "",
   requestTimeoutMs: 12000,
-  appVersion: "1.1.2",
+  appVersion: "1.1.3",
   releaseDates: {}
 };
